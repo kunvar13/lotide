79acfd015bb28cf3d0ce8edbde86231b1a782c5c
kunvar13 <kunvar13@gmail.com>
2021-10-26 22:29:38 -0600

    Code for without assignment - TNeed to add test case

---
+++ without.js
@@ -0,0 +1,25 @@
+const without = function(array1, array2) {
+  let indToRemove = [];
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        indToRemove.push(i);
+      }
+      
+    }
+  }
+  const modArray = function(indToRemove) {
+    for (let k = 0; k < indToRemove.length; k++) {
+      array1.splice(indToRemove[k],1);
+    }
+    return (array1);
+  };
+
+  modArray(indToRemove);
+  //return array1;
+};
+
+console.log((without([1, 2, 3], [1]), [2,3])); // => [2, 3]
+console.log((without(["1", "2", "3"], [1, 2, "3"]),["1","2"])); // => ["1", "2"]
+
+// test case to be added
