beb2f5d6137102d7f373cd8908bffa4aeb67998e
kunvar13 <kunvar13@gmail.com>
2021-10-28 17:48:20 -0600

    Made a correction in assertArrayequal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,13 @@
+const asserArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    return `✅✅✅ Assertion Passed ${array1} === ${array2}`;
+  } else {
+    return `🛑🛑🛑Assertion failed ${array1} !== ${array2}`;
+  }
+};
+
+
+
 const eqArrays = function(array1, array2) {
   let num = 0;
   if (array1.length === array2.length) {
@@ -7,16 +17,16 @@ const eqArrays = function(array1, array2) {
       }
     }
   } else {
-    console.log(`🛑🛑🛑Assertion failed ${array1} !== ${array2}`);
+    return false;
   }
   if (num === 0) {
-    console.log(`✅✅✅ Assertion Passed ${array1} === ${array2}`);
+    return true;
   } else {
-    console.log(`🛑🛑🛑Assertion failed ${array1} !== ${array2}`);
+    return false;
   }
 };
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]);// => false
+console.log(asserArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(asserArraysEqual([1, 2, 3], [3, 2, 1]));// => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+console.log(asserArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(asserArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
--- test.js
+++ test.js
@@ -1,77 +1,16 @@
-//function to find out size of an Object
-const sizeObj = function(obj1) {
+const raisinAlarm = function(cookie) {
   let num = 0;
-  for (let i in obj1) {
-    num++;
-  }
-  return num;
-};
-
-//function to comapre the two objects with arrays inside
-
-const eqArrays = function(array1, array2) {
-  let num = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        num++;
-      }
+  for (let i = 0; i < cookie.length; i++) {
+    if (cookie[i] === "🍇") {
+      num++;
     }
-  } else {
-    return false;
   }
-  if (num === 0) {
-    return true;
+  if (num > 0) {
+    console.log("Raisin Alert");
   } else {
-    return false;
+    console.log("All good!");
   }
 };
-
-const eqObjects = function(obj1, obj2) {
-  let num = 0;
-  let kal1;
-  let kal2;
-  //let result;
-  if (sizeObj(obj1) === sizeObj(obj2)) {
-    for (let i in obj1) {
-      for (let j in obj2) {
-        kal1 = typeof obj1[i];
-        kal2 = typeof obj2[j];
-        if (kal1 === kal2) {
-          if (kal1 === 'object') {
-            if (eqArrays(obj1[i], obj2[j]) === true) {
-              num++;
-            }
-          } else if (obj1[i] === obj2[j]) {
-            num++;
-          }
-        } else {
-          continue;
-        }
-      }
-    }
-    if (num === sizeObj(obj1)) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
-};
-
-
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
