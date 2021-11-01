c2504df7c9814824112c21de760042712f30d649
kunvar13 <kunvar13@gmail.com>
2021-11-01 13:41:20 -0600

    Adding assetArraysequal with testing

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2) === true) {
     return `✅✅✅ Assertion Passed ${array1} === ${array2}`;
@@ -6,27 +8,4 @@ const assertArraysEqual = function (array1, array2) {
   }
 };
 
-
-
-const eqArrays = function(array1, array2) {
-  let num = 0;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        num++;
-      }
-    }
-  } else {
-    return false;
-  }
-  if (num === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));// => false
-
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
+module.exports = assertArraysEqual;
